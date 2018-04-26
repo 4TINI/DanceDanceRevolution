50d5a6facf36a228059cc08f4762f41d536b662f
geronimaw <38426513+geronimaw@users.noreply.github.com>
2018-04-26 11:19:26 +0200

    Add files via upload

---
+++ istruzioni.ino
@@ -0,0 +1,7 @@
+% in ordine:
+% aprire StandardFirmata e caricarlo su Arduino
+% fare prova con arduino_input su Processing: collegate i pin
+% a 3v o 5v e si colorano i rettangolini nella finestra che 
+% processing apre. è solo una prova per vedere se avete effettivamente
+% collegato arduino e processing.
+% prova_con_media è il codice per il capacitivo ma non comunica con processing
